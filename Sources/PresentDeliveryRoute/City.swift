c50f4658ee2d0c154bbf13295165630340cb9da4
David Ludlow <dludlow7@ford.com>
2020-12-18 18:46:29 +0000

    Biggest City refactored to an extension

--- Sources/PresentDeliveryRoute/City.swift
+++ Sources/PresentDeliveryRoute/City.swift
@@ -22,12 +22,15 @@ class City: Equatable {
 func getRoute(_ cities: [City]) -> [City] {
     var route: [City] = []
 
-    cities.sorted { (city1, city2) -> Bool in
-        return city1.population > city2.population
-    }
-    .first
-    .map { firstCity -> Void in
-        route.append(firstCity)
-    }
+    route.append(cities.biggest)
     return route
+}
+
+extension Array where Element: City {
+    var biggest: City {
+        return self.sorted { (city1, city2) -> Bool in
+            return city1.population > city2.population
+        }
+        .first!
+    }
 }
